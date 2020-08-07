81f626e641410678aa7d426a314fb5503fd1bb95
patrick-replogle <patrickr1138@gmail.com>
2020-08-07 10:28:26 -0700

    added solutions to moving zeroes and single number problems

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,48 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+# third pass solution
+def moving_zeroes(arr):
+    arr.sort(reverse=True)
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            arr[i], arr[len(arr) - 1] = arr[len(arr) - 1], arr[i]
+    return arr
+
+
+# first pass solution
+# def moving_zeroes(arr):
+#     positive = []
+#     negative = []
+#     zero = []
+
+#     for i in range(len(arr)):
+#         if arr[i] > 0:
+#             positive.append(arr[i])
+#         elif arr[i] < 0:
+#             negative.append(arr[i])
+#         else:
+#             zero.append(arr[i])
+
+#     return positive + negative + zero
+
+
+# second pass
+# def moving_zeroes(arr):
+#     for i, val in enumerate(arr):
+#         if val > 0:
+#             arr.pop(i)
+#             arr.insert(0, val)
+#         elif val == 0:
+#             arr.pop(i)
+#             arr.append(val)
+#     return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 3, 1, 0, -2]  # => [3, 1, -2, 0, 0]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,12 +2,27 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
+# first solution
 def single_number(arr):
-    # Your code here
+    for val in arr:
+        if arr.count(val) == 1:
+            return val
 
-    pass
+
+# second solution
+# def single_number(arr):
+#     unique = set()
+#     for val in arr:
+#         if val in unique:
+#             unique.remove(val)
+#         else:
+#             unique.add(val)
+#     return unique.pop()
 
 
+print(single_number([1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]))
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
