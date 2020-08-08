0d04b3ed19e7e356f189d32362d9c50d4ae422de
patrick-replogle <patrickr1138@gmail.com>
2020-08-07 18:48:33 -0700

    mvp for day one

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,29 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+cache = {}
+
+
+def eating_cookies(n, memoize=None):
+    if n in cache:
+        return cache[n]
+    else:
+        if n == 0:
+            return 1
+        if n < 0:
+            return 0
+
+        result = eating_cookies(
+            n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        cache[n] = result
+        return cache[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,57 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+# first pass solution works, but doesn't perform well with large data sets
+# def sliding_window_max(nums, k):
+#     result = []
+
+#     for i in range(len(nums) - k + 1):
+#         result.append(max(nums[i: i + k]))
+
+#     return result
+
+
+# another slow solution with large data sets
+# def sliding_window_max(nums, k):
+#     result = []
+
+#     for i in range(len(nums) - k + 1):
+#         max_num = nums[i]
+#         j = i
+#         while j < i + k:
+#             if nums[j] > max_num:
+#                 max_num = nums[j]
+#             j += 1
+#         result.append(max_num)
+
+#     return result
+
+
+def sliding_window_max(nums, k):
+    queue = []
+    results = []
+    m = 0
+
+    for i in range(len(nums)):
+        while len(queue) > m and nums[queue[-1]] <= nums[i]:
+            queue.pop()
+
+        queue.append(i)
+
+        if i >= k-1:
+            results.append(nums[queue[m]])
+            if i - queue[m] >= k - 1:
+                m += 1
+
+    return results
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
+    expected_output = [3, 3, 5, 5, 6, 7]
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
